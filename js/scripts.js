804d13cf731c46540f4e8ef98435519611c0a8fd
Shadman Chowdhury <shadmanchowdhury@Shadmans-MBP.home>
2018-06-03 21:43:55 -0400

    index.html almost done

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 /*global $*/
 /*global ready*/
 $(document).ready(function(){
-
+  $("#slideshow").backstretch([
+      {url: "./media/img1.jpg"},
+      {url: "./media/img2.jpg"},
+      {url: "./media/img3.jpg"},
+      {url: "./media/img4.jpg"}
+    ], {
+        duration: 4000,
+        alignY:0.7,
+        transition: ["fade"],
+        transitionDuration: 1000
+    });
+    $(".scrolla").scrolla();
 })
