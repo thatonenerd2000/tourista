80d4173d5c8d298dddad6ce8ba8a6f3510b14111
Shadman Chowdhury <shadmanchowdhury@Shadmans-MBP.home>
2018-06-10 14:24:33 -0400

    almost done

--- js/find.js
+++ js/find.js
@@ -1,8 +1,23 @@
+$(document).ready(function(){
+  $(".scrolla").scrolla();
+})
+
 var map;
 var pos;
 var marker;
 var pic = document.getElementById("pic")
 var service;
+var contacttext;
+
+function fade(element) {
+   var op = 0;  // initial opacity
+   var timer = setInterval(function () {
+       element.style.opacity = op;
+       element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+       op += 0.1;
+   }, 50);
+}
+
 function initMap(){
   map = new google.maps.Map(
     document.getElementById('map'),
@@ -24,6 +39,7 @@ function initMap(){
         position:pos,
         map:map,
         title:"You are here :)",
+        icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
       })
       var httpReq = new XMLHttpRequest();
       var url = "https://cors.io/?https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+pos.lat+","+pos.lng+"&radius=5000&type=places+to+visit+near+me&key=AIzaSyBjltKonYN1BdEJTfJXAxt5mOd-tleO1tw";
@@ -32,7 +48,7 @@ function initMap(){
       var finalobj = JSON.parse(httpReq.responseText);
       console.log(finalobj.results);
 
-      for(var i = 0; i < 8; i++){
+      for(var i = 0; i < 9; i++){
         var request = {
           placeId: (finalobj.results[i].place_id)
         };
@@ -43,28 +59,56 @@ function initMap(){
         function callback(place, status) {
           if (status == google.maps.places.PlacesServiceStatus.OK) {
             console.log(place);
-              var img = new Image();
-              img.src = place.photos[1].getUrl({'maxWidth': 1000, 'maxHeight': 1200});
-              pic.append(img);
-              $(document).ready(function(){
-                $(img).hover(function(){
-                  $(this).css("opacity","0.5")
-                },function(){
-                  $(this).css("opacity", "1");})
-              })
+            var img = new Image();
+            var img2 = new Image();
+            var img3 = new Image();
+            img.src = place.photos[1].getUrl({'maxWidth': 1000, 'maxHeight': 1000});
+            img2.src = place.photos[2].getUrl({'maxWidth': 1000, 'maxHeight': 1000});
+            img3.src = place.photos[3].getUrl({'maxWidth': 1000, 'maxHeight': 1000});
+            pic.append(img);
+            pic.append(img2);
+            pic.append(img3);
+            var h = document.createElement("H1");
+            var add = document.createElement("P");
+            var contact = document.createElement("P");
+            if(place.formatted_phone_number != undefined){
+              contacttext = document.createTextNode("Contacto: " + place.formatted_phone_number);
+            }
+            else{
+              contacttext = "";
+            }
+            var addt = document.createTextNode("Dirección: " + place.formatted_address);
+            var hr = document.createElement("HR");
+            var t = document.createTextNode(place.name);
+            hr.style.display = "block";
+            hr.style.border = "2px solid";
+            contact.append(contacttext);
+            h.appendChild(t);
+            add.appendChild(addt);
+            pic.append(h);
+            pic.append(add);
+            pic.append(contact);
+            pic.append(hr);
+            h.style.position = "relative"
+            map.setZoom(12);
+            var mark = new google.maps.Marker({
+              position:place.geometry.location,
+              map:map,
+              animation: google.maps.Animation.DROP,
+            })
+            mark.addListener('click', toggleBounce);
+            function toggleBounce() {
+              if (mark.getAnimation() !== null) {
+                mark.setAnimation(null);
+              } else {
+                mark
+                .setAnimation(google.maps.Animation.BOUNCE);
+              }
+            }
+            fade(img);
           }
         }
       }
-      // console.log(finalobj.results[0].photos[0].photo_reference);
-      //
-      // for(var i = 0; i < 8; i++){
-      //   imghttp.open('GET',"https://cors.io/?https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference="+ finalobj.results[i].photos[0].photo_reference +"&key=AIzaSyBjltKonYN1BdEJTfJXAxt5mOd-tleO1tw",true);
-      //   imghttp.send();
-      //   console.log(imghttp);
-      //   var img = new Image();
-      //   img.src = imghttp.response;
-      //   content.append(img);
-      // }
     })
   }
 }
